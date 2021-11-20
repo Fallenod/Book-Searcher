6ace26fb427993f0bdbca64300cd8d5263a08fbc
Fallenod <thefallenod@gmail.com>
2021-11-20 16:25:55 +0500

    adD PRISE data to bookcard

--- src/components/BookCard/BookCard.js
+++ src/components/BookCard/BookCard.js
@@ -1,3 +1,4 @@
+import {Link} from 'react-router-dom';
 import style from './BookCard.module.css';
 import Loader from '../../components/Loader/Loader';
 import noImage from './noImage.svg'
@@ -7,17 +8,24 @@ function BookCard(items) {
     return ( 
         <Fragment>
         {
-            items.items ? items.items.map(({volumeInfo}) => (
+            items.items ? items.items.map(({id, volumeInfo, saleInfo }) => (
                 <div className={style.mainItem}>
-                    <div className={style.imageWrapper}>
-                        <img  src={
-                            volumeInfo.imageLinks === undefined
-                                ? `${noImage}`
-                                : `${volumeInfo.imageLinks.thumbnail}`
-                        } alt="" loading="lazy" />
-                    </div>
-                    <p className={style.textTitle}>{volumeInfo.title}</p>
+                    <Link key={id} to={`/book/${id}`}>
+                        <div className={style.imageWrapper}>
+                            <img  src={
+                                volumeInfo.imageLinks === undefined
+                                    ? `${noImage}`
+                                    : `${volumeInfo.imageLinks.thumbnail}`
+                            } alt="" loading="lazy" />
+                        </div>
+                        <p className={style.textTitle}>{volumeInfo.title}</p>
+                    </Link>
                     <p className={style.textAuthors}>{volumeInfo.authors}</p>
+                    <p className={style.textAuthors}>{
+                        saleInfo.saleability === 'NOT_FOR_SALE'
+                            ? `not for sale` : `${saleInfo.listPrice.amount}`
+                        }
+                    </p>
                 </div>
             )) : <Loader/>
         }
