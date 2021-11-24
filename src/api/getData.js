aa85f6bb070379f2cb7ec6f632f9a0364312c2b3
Fallenod <thefallenod@gmail.com>
2021-11-24 16:46:17 +0500

    delete ffc

--- src/api/getData.js
+++ src/api/getData.js
@@ -5,7 +5,12 @@ export const getData = async (value = "", index = 1, max = 20) => {
         const res = await fetch(url);
         const result = await res.json()
         if(result.status >= 400) {
-            throw Error('Something wrong')
+            throw Error('Something wrong')  
         }
-    return result
+        return result
 }
+
+
+
+
+
