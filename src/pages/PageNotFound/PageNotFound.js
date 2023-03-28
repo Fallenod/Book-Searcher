f851264794de95614fa492b7851173cf8e0603a2
Марсель Галиакберов <thefallenod@gmail.com>
2023-03-28 19:19:36 +0500

    reuse

--- src/pages/PageNotFound/PageNotFound.js
+++ src/pages/PageNotFound/PageNotFound.js
@@ -4,7 +4,7 @@ import logo404 from './404.gif';
 function PageNotFound() {
     return ( 
         <div className={style.main}>
-            <img src={logo404} alt="logo" />
+            <img className={style.notFoundPageImg} src={logo404} alt="logo" />
         </div>
      );
 }
